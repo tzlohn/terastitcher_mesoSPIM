0f550c9488a0aa9edddb924c78950520c4a17f31
tzlohn <57408685+tzlohn@users.noreply.github.com>
2020-09-09 16:57:21 +0200

    going to edit xml for XY

--- mit.py
+++ mit.py
@@ -3,6 +3,41 @@ from Channel_sorting import sortChannel
 from LR_sorting import sortLR
 import sys,os,re
 
+def find_key_from_meta(all_line_string,key):
+    a_line = "nothing should be the same"
+    n = -1
+    while a_line != key and n < len(all_line_string):
+        n = n+1
+        pattern = re.compile(r"[\[](%s)[\]] \:(.*)?\n"%key)
+        a_line_all = pattern.findall(all_line_string[n])
+        if not a_line_all:
+            a_line = "nothing should be the same"
+        else: 
+            a_line = a_line_all[0][0]
+            value = a_line_all[0][1]
+    
+    if not value:
+        return [n,"not_a_value"] 
+    else:    
+        return [n,value]
+
+def edit_meta(metaFile,key,value):
+    new_line = "["+ key + "]" + " : " + str(value) +"\n"
+    meta = open(metaFile,"r")
+    all_lines = meta.readlines()
+    meta.close()
+    [line_sn,value] = find_key_from_meta(all_lines,key)
+    all_lines[line_sn] = new_line
+
+    if line_sn < len(all_lines):
+        with open(metaFile,"w") as meta:
+            meta.writelines(all_lines)
+    """        
+    elif line_sn == len(all_lines):
+        with open(self.metaFile,"a") as meta:
+            meta.writelines(all_lines)
+    """
+
 class LR_GroupBox(QtWidgets.QGroupBox):
     def __init__(self,parent = None, side = None):
         super().__init__(parent)
@@ -16,6 +51,7 @@ class LR_GroupBox(QtWidgets.QGroupBox):
 
         self.XYStitchButton = QtWidgets.QPushButton(self)
         self.XYStitchButton.setText("generate xml and stitch")
+        self.XYStitchButton.clicked.connect(self.XYstitch)
 
         self.grouplayout = QtWidgets.QGridLayout()
         self.grouplayout.addWidget(self.unstitchedFileLabel,0,0)
@@ -26,17 +62,24 @@ class LR_GroupBox(QtWidgets.QGroupBox):
     
     def askdirectory(self):
         self.SortedFileLocation = QtWidgets.QFileDialog.getExistingDirectory(self)
-        self.unstitchedFileLocation.setText(self.SortedFileLocation)        
+        self.unstitchedFileLocation.setText(self.SortedFileLocation)
+
+    def XYstitch(self):
+        pass        
 
 class DVTab(QtWidgets.QWidget):
     def __init__(self,parent = None, DV = None):
         super().__init__(parent)
-        
+        self.pars_channelTab = parent
+        self.DV = DV
+
         self.LeftBox = LR_GroupBox(self,side = "Left")
         self.LeftBox.setTitle("Left")
+        self.LeftBox.setDisabled(True)
 
         self.RightBox = LR_GroupBox(self,side = "Right")
         self.RightBox.setTitle("Right")
+        self.RightBox.setDisabled(True)
 
         self.LRMergeBox = QtWidgets.QGroupBox(self)
         self.LRMergeBox.setTitle("Left-Right merge")
@@ -44,12 +87,13 @@ class DVTab(QtWidgets.QWidget):
         self.RawFileLabel = QtWidgets.QLabel("raw file directory")
         self.RawFileLocation = QtWidgets.QLineEdit(self)
 
-        with open(self.parent().parent().parent().metaFile,"r") as meta:
-            wanted_line = DV + " raw file"
+        with open(self.pars_channelTab.pars_initWindow.metaFile,"r") as meta:
+            self.current_line = self.DV + " raw file"
             all_lines = meta.readlines()
-            [SN,value] = self.parent.parent.parent.find_key_from_meta(all_lines,wanted_line)
-            if value != "False":
-                self.RawFileLocation.setText(value)
+            [SN,file_location] = find_key_from_meta(all_lines,self.current_line)
+            if file_location != "Not assigned":
+                self.RawFileLocation.setText(file_location)
+                self.file_location = file_location
         
         self.RawFileLocation.setReadOnly(True)
         self.reloadUnsortedfilebutton = QtWidgets.QPushButton(self)
@@ -57,7 +101,7 @@ class DVTab(QtWidgets.QWidget):
         self.reloadUnsortedfilebutton.clicked.connect(self.askdirectory)
 
         self.LRSplitButton = QtWidgets.QPushButton(self)
-        self.LRSplitButton.setText("Split Left and Right")
+        self.LRSplitButton.setText("Split Left and Right, and save to tiff")
         self.LRSplitButton.clicked.connect(self.splitLR)
 
         self.tabLayout = QtWidgets.QGridLayout()
@@ -71,17 +115,29 @@ class DVTab(QtWidgets.QWidget):
         self.setLayout(self.tabLayout)
 
     def askdirectory(self):
-        self.unSortedFileLocation = QtWidgets.QFileDialog.getExistingDirectory(self)
-        self.RawFileLocation.setText(self.unSortedFileLocation)        
+        self.file_location = QtWidgets.QFileDialog.getExistingDirectory(self)
+        self.RawFileLocation.setText(self.file_location)
+        edit_meta(self.pars_channelTab.pars_initWindow.metaFile,self.current_line,self.file_location)        
 
     def splitLR(self):
-        #sortLR(os.getcwd())    
-        return 0    
-
+        #sortLR(self.file_location)
+        key_left = self.DV+" left file"
+        key_right = self.DV+" right file"
+        self.left_location = self.file_location + "/Left"
+        self.right_location = self.file_location + "/Right"
+        edit_meta(self.pars_channelTab.pars_initWindow.metaFile,key_left,self.left_location)
+        edit_meta(self.pars_channelTab.pars_initWindow.metaFile,key_right,self.right_location)
+        self.LeftBox.unstitchedFileLocation.setText(self.left_location)
+        self.RightBox.unstitchedFileLocation.setText(self.right_location)
+        self.LeftBox.setDisabled(False)
+        self.RightBox.setDisabled(False)
+          
 class ChannelTab(QtWidgets.QWidget):
     def __init__(self,parent = None):
         super().__init__(parent)
+        self.pars_mainWindow = parent
         self.DVtabs = QtWidgets.QTabWidget(parent=self)
+        
         sides = ["ventral","dorsal"]
         for side in sides:
             self.DVtabs.addTab(DVTab(parent, DV = side),side)
@@ -94,6 +150,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Stitching workpanel")
         self.channel_tabs = QtWidgets.QTabWidget(parent=self)
 
+        self.pars_initWindow = parent
         current_folder = os.getcwd()
         channel_folders = self.splitChannels()
         for a_channel_folder in channel_folders:
@@ -153,7 +210,7 @@ class InitWindow(QtWidgets.QWidget):
         self.filename = self.askFilename.text()
         self.metaFile = self.prepare_meta(self.filename)
         if self.side == "ventral":
-            self.edit_meta("ventral raw file",self.DataFolder)
+            edit_meta(self.metaFile,"ventral raw file",self.DataFolder)
         elif self.side == "dorsal":
             self.edit_meta("dorsal raw file",self.DataFolder)       
         self.prerequisiteWidget.hide()
@@ -176,44 +233,15 @@ class InitWindow(QtWidgets.QWidget):
             meta.write("[x positions_Left] :\n")
             meta.write("=== progress ===\n")
             meta.write("=== file location ===\n")
-            meta.write("[ventral raw file] : False\n")
-            meta.write("[dorsal raw file] : False\n")
+            meta.write("[ventral raw file] : Not assigned\n")
+            meta.write("[ventral left file] : Not assigned\n")
+            meta.write("[ventral right file] : Not assigned\n")
+            meta.write("[dorsal raw file] : Not assigned\n")
+            meta.write("[dorsal left file] : Not assigned\n")
+            meta.write("[dorsal right file] : Not assigned\n")
+
         return meta_name
     
-    def edit_meta(self,key,value):
-        new_line = "["+ key + "]" + " : " + str(value) +"\n"
-        meta = open(self.metaFile,"r")
-        all_lines = meta.readlines()
-        meta.close()
-        [line_sn,value] = self.find_key_from_meta(all_lines,key)
-        all_lines[line_sn] = new_line
-
-        if line_sn < len(all_lines):
-            with open(self.metaFile,"w") as meta:
-                meta.writelines(all_lines)
-        """        
-        elif line_sn == len(all_lines):
-            with open(self.metaFile,"a") as meta:
-                meta.writelines(all_lines)
-        """
-    def find_key_from_meta(self,all_line_string,key):
-        a_line = "nothing should be the same"
-        n = -1
-        while a_line != key and n < len(all_line_string):
-            n = n+1
-            pattern = re.compile(r"[\[](%s)[\]] \:(.*)?\n"%key)
-            a_line_all = pattern.findall(all_line_string[n])
-            if not a_line_all:
-                a_line = "nothing should be the same"
-            else: 
-                a_line = a_line_all[0][0]
-                value = a_line_all[0][1]
-        
-        if not value:
-            return [n,"not_a_value"] 
-        else:    
-            return [n,value]
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = InitWindow()
--- xml_XY_stitching.py
+++ xml_XY_stitching.py
@@ -29,153 +29,156 @@ def get_value(expression,text):
     value = float(value)
     return value
 
-root = tk.Tk()
-root.withdraw()
+def xml_XY(folderpath):
+    # create a new folder for future Left right fusion in the upper directory/folder
+    pattern = re.compile(r"(.*./)\w+$")
+    root_folder = pattern.findall(folderpath)
+    root_folder = root_folder[0]
+    os.chdir(root_folder)
+    if os.path.exists("LR_fusion"):
+        pass
+    else:
+        os.mkdir("LR_fusion")
+    os.chdir(folderpath)
 
-folderpath = filedialog.askdirectory()
+    # find out the illumination side
+    pattern = re.compile(r"(.*./)(Left|Right)(./.*./)?")
+    illumination_side = pattern.findall(folderpath)
+    illumination_side = illumination_side[0][1] 
 
-# create a new folder for future Left right fusion in the upper directory/folder
-pattern = re.compile(r"(.*./)\w+$")
-root_folder = pattern.findall(folderpath)
-root_folder = root_folder[0]
-os.chdir(root_folder)
-if os.path.exists("LR_fusion"):
-    pass
-else:
-    os.mkdir("LR_fusion")
-os.chdir(folderpath)
+    xml_name = folderpath + "\\" + "terastitcher_for_XY" + ".xml"
+    #meta_name = "meta_for_LR_fusion" + ".txt"
 
-# find out the illumination side
-pattern = re.compile(r"(.*./)(Left|Right)(./.*./)?")
-illumination_side = pattern.findall(folderpath)
-illumination_side = illumination_side[0][1] 
+    file_list = glob.glob('*tif_meta.txt')
+    pattern_y = re.compile(r"X-?\d+_Y(-?\d+)")
+    pattern_x = re.compile(r"X(-?\d+)_Y-?\d+")
 
-xml_name = folderpath + "\\" + "terastitcher_for_XY" + ".xml"
-meta_name = "meta_for_LR_fusion" + ".txt"
+    # Counting number of tiles, getting the offset between them
+    y_pos_all = []
+    x_pos_all = []
+    for a_file_name in file_list:
+        y_pos = float(pattern_y.findall(a_file_name)[0])
+        if y_pos not in y_pos_all:
+            y_pos_all.append(y_pos)
+        x_pos = float(pattern_x.findall(a_file_name)[0])
+        if x_pos not in x_pos_all:
+            x_pos_all.append(x_pos)
+    y_pos_all.sort(reverse= True)
+    x_pos_all.sort(reverse= True)
 
-file_list = glob.glob('*tif_meta.txt')
-pattern_y = re.compile(r"X-?\d+_Y(-?\d+)")
-pattern_x = re.compile(r"X(-?\d+)_Y-?\d+")
+    total_row = len(y_pos_all)
+    total_column = len(x_pos_all)
 
-# Counting number of tiles, getting the offset between them
-y_pos_all = []
-x_pos_all = []
-for a_file_name in file_list:
-    y_pos = float(pattern_y.findall(a_file_name)[0])
-    if y_pos not in y_pos_all:
-        y_pos_all.append(y_pos)
-    x_pos = float(pattern_x.findall(a_file_name)[0])
-    if x_pos not in x_pos_all:
-        x_pos_all.append(x_pos)
-y_pos_all.sort(reverse= True)
-x_pos_all.sort(reverse= True)
+    # Terasticher requires users to put the file in a order of the aligning/stitching direction
+    new_file_index = []
+    for a_file_name in file_list:
+        y_pos = float(pattern_y.findall(a_file_name)[0])
+        x_pos = float(pattern_x.findall(a_file_name)[0])
+        #index = y_pos_all.index(y_pos) + x_pos_all.index(x_pos)*total_row
+        index = x_pos_all.index(x_pos) + y_pos_all.index(y_pos)*total_column
+        new_file_index.append(index)
+    sorted_file_list = [file_list for _, file_list in sorted(zip(new_file_index, file_list))]
 
-total_row = len(y_pos_all)
-total_column = len(x_pos_all)
+    if total_row > 1:
+        offset_V = y_pos_all[0]-y_pos_all[1]
+    else:
+        offset_V = 0
 
-# Terasticher requires users to put the file in a order of the aligning/stitching direction
-new_file_index = []
-for a_file_name in file_list:
-    y_pos = float(pattern_y.findall(a_file_name)[0])
-    x_pos = float(pattern_x.findall(a_file_name)[0])
-    #index = y_pos_all.index(y_pos) + x_pos_all.index(x_pos)*total_row
-    index = x_pos_all.index(x_pos) + y_pos_all.index(y_pos)*total_column
-    new_file_index.append(index)
-sorted_file_list = [file_list for _, file_list in sorted(zip(new_file_index, file_list))]
+    if total_column > 1:
+        offset_H = x_pos_all[0]-x_pos_all[1]
+    else:
+        offset_H = 0
 
-if total_row > 1:
-    offset_V = y_pos_all[0]-y_pos_all[1]
-else:
-    offset_V = 0
+    with open(file_list[0],'r') as a_metafile:
+        im_info = a_metafile.read()
+        pattern = re.compile(r"[\[]Pixelsize in um[\]] (\d+)(\.)?(\d+)?")
+        dim_V = get_value(pattern,im_info)
+        dim_H = dim_V
+        
+        pattern = re.compile(r"[\[]z_stepsize[\]] (\d+)(\.)?(\d+)?")
+        dim_D = get_value(pattern,im_info)
 
-if total_column > 1:
-    offset_H = x_pos_all[0]-x_pos_all[1]
-else:
-    offset_H = 0
+        pattern = re.compile(r"[\[]z_planes[\]] (\d+)")
+        output = pattern.findall(im_info)
+        slice_no = int(output[0])
 
-with open(file_list[0],'r') as a_metafile:
-    im_info = a_metafile.read()
-    pattern = re.compile(r"[\[]Pixelsize in um[\]] (\d+)(\.)?(\d+)?")
-    dim_V = get_value(pattern,im_info)
-    dim_H = dim_V
-    
-    pattern = re.compile(r"[\[]z_stepsize[\]] (\d+)(\.)?(\d+)?")
-    dim_D = get_value(pattern,im_info)
+        pattern = re.compile(r"[\[]x_pixels[\]] (\d+)")
+        x_pixel_count = get_value(pattern,im_info)
 
-    pattern = re.compile(r"[\[]z_planes[\]] (\d+)")
-    output = pattern.findall(im_info)
-    slice_no = int(output[0])
+        pattern = re.compile(r"[\[]y_pixels[\]] (\d+)")
+        y_pixel_count = get_value(pattern,im_info)     
 
-    pattern = re.compile(r"[\[]x_pixels[\]] (\d+)")
-    x_pixel_count = get_value(pattern,im_info)
+    with open(xml_name,'w') as xml_file:
+        xml_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n")
+        xml_file.write("<!DOCTYPE TeraStitcher SYSTEM \"TeraStitcher.DTD\">\n")
+        xml_file.write("<TeraStitcher volume_format=\"TiledXY|3Dseries\" input_plugin=\"tiff3D\">\n")
+        xml_file.write("    <stacks_dir value=\"%s\" />\n"%folderpath)
+        xml_file.write("    <ref_sys ref1=\"%d\" ref2=\"%d\" ref3=\"%d\" />\n"%(ref1,ref2,ref3))
+        xml_file.write("    <voxel_dims V=\"%.2f\" H=\"%.2f\" D=\"%.2f\" />\n"%(dim_V,dim_H,dim_D))
+        xml_file.write("    <origin V=\"%.3f\" H=\"%.3f\" D=\"%.3f\" />\n"%(ori_V,ori_H,ori_D))
+        xml_file.write("    <mechanical_displacements V=\"%.2f\" H=\"%.2f\" />\n"%(offset_V,offset_H))
+        xml_file.write("    <dimensions stack_rows=\"%d\" stack_columns=\"%d\" stack_slices=\"%d\" />\n"%(total_row,total_column,slice_no))
+        xml_file.write("    <STACKS>\n")
+        for file_name in sorted_file_list:
+            with open(file_name,'r') as this_im:
+                im_info = this_im.read()
+                xml_file.write("        <Stack N_BLOCKS=\"1\"")
+                xml_file.write(" BLOCK_SIZES=\"%.2f\""%(slice_no*dim_D))
+                xml_file.write(" BLOCKS_ABS_D=\"0\"")
+                xml_file.write(" N_CHANS=\"1\"")
+                xml_file.write(" N_BYTESxCHAN=\"%d\""%(bit/8))
+                
+                pattern = re.compile(r"[\[]y_pos[\]] (-)?(\d+)(\.)?(\d+)?")
+                y_position = int(round(get_value(pattern,im_info)))
+                y_index = y_pos_all.index(y_position)
 
-    pattern = re.compile(r"[\[]y_pixels[\]] (\d+)")
-    y_pixel_count = get_value(pattern,im_info)     
+                
+                pattern = re.compile(r"[\[]x_pos[\]] (-)?(\d+)(\.)?(\d+)?")
+                x_position = int(round(get_value(pattern,im_info)))
+                x_index = x_pos_all.index(x_position)
 
-with open(xml_name,'w') as xml_file:
-    xml_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n")
-    xml_file.write("<!DOCTYPE TeraStitcher SYSTEM \"TeraStitcher.DTD\">\n")
-    xml_file.write("<TeraStitcher volume_format=\"TiledXY|3Dseries\" input_plugin=\"tiff3D\">\n")
-    xml_file.write("    <stacks_dir value=\"%s\" />\n"%folderpath)
-    xml_file.write("    <ref_sys ref1=\"%d\" ref2=\"%d\" ref3=\"%d\" />\n"%(ref1,ref2,ref3))
-    xml_file.write("    <voxel_dims V=\"%.2f\" H=\"%.2f\" D=\"%.2f\" />\n"%(dim_V,dim_H,dim_D))
-    xml_file.write("    <origin V=\"%.3f\" H=\"%.3f\" D=\"%.3f\" />\n"%(ori_V,ori_H,ori_D))
-    xml_file.write("    <mechanical_displacements V=\"%.2f\" H=\"%.2f\" />\n"%(offset_V,offset_H))
-    xml_file.write("    <dimensions stack_rows=\"%d\" stack_columns=\"%d\" stack_slices=\"%d\" />\n"%(total_row,total_column,slice_no))
-    xml_file.write("    <STACKS>\n")
-    for file_name in sorted_file_list:
-        with open(file_name,'r') as this_im:
-            im_info = this_im.read()
-            xml_file.write("        <Stack N_BLOCKS=\"1\"")
-            xml_file.write(" BLOCK_SIZES=\"%.2f\""%(slice_no*dim_D))
-            xml_file.write(" BLOCKS_ABS_D=\"0\"")
-            xml_file.write(" N_CHANS=\"1\"")
-            xml_file.write(" N_BYTESxCHAN=\"%d\""%(bit/8))
-            
-            pattern = re.compile(r"[\[]y_pos[\]] (-)?(\d+)(\.)?(\d+)?")
-            y_position = int(round(get_value(pattern,im_info)))
-            y_index = y_pos_all.index(y_position)
+                xml_file.write(" ROW=\"%d\""%(y_index))
+                xml_file.write(" COL=\"%d\""%(x_index))   
+                xml_file.write(" ABS_H=\"%.1f\""%(round(x_position/dim_H)))
+                xml_file.write(" ABS_V=\"%.1f\""%(round(y_position/dim_V)))
+                
+                xml_file.write(" ABS_D=\"0\"")
+                xml_file.write(" STITCHABLE=\"yes\"")
+                xml_file.write(" DIR_NAME=\"\"")
+                xml_file.write(" Z_RANGES=\"[0,%d)\""%(slice_no))
+                
+                
+                pattern = re.compile(r"(.*)_meta.txt")
+                image_name = pattern.findall(file_name)[0]           
+                print(image_name)
+                xml_file.write(" IMG_REGEX=\"%s\">\n"%(image_name))
+                
+                xml_file.write("            <NORTH_displacements/>\n")
+                xml_file.write("            <EAST_displacements/>\n")
+                xml_file.write("            <SOUTH_displacements/>\n")
+                xml_file.write("            <WEST_displacements/>\n")
+                xml_file.write("        </Stack>\n")
+        xml_file.write("    </STACKS>\n")
+        xml_file.write("</TeraStitcher>\n")
 
-            
-            pattern = re.compile(r"[\[]x_pos[\]] (-)?(\d+)(\.)?(\d+)?")
-            x_position = int(round(get_value(pattern,im_info)))
-            x_index = x_pos_all.index(x_position)
+    fusion_folder = root_folder + "/LR_fusion/"
+    os.chdir(fusion_folder)
 
-            xml_file.write(" ROW=\"%d\""%(y_index))
-            xml_file.write(" COL=\"%d\""%(x_index))   
-            xml_file.write(" ABS_H=\"%.1f\""%(round(x_position/dim_H)))
-            xml_file.write(" ABS_V=\"%.1f\""%(round(y_position/dim_V)))
-            
-            xml_file.write(" ABS_D=\"0\"")
-            xml_file.write(" STITCHABLE=\"yes\"")
-            xml_file.write(" DIR_NAME=\"\"")
-            xml_file.write(" Z_RANGES=\"[0,%d)\""%(slice_no))
-            
-            
-            pattern = re.compile(r"(.*)_meta.txt")
-            image_name = pattern.findall(file_name)[0]           
-            print(image_name)
-            xml_file.write(" IMG_REGEX=\"%s\">\n"%(image_name))
-            
-            xml_file.write("            <NORTH_displacements/>\n")
-            xml_file.write("            <EAST_displacements/>\n")
-            xml_file.write("            <SOUTH_displacements/>\n")
-            xml_file.write("            <WEST_displacements/>\n")
-            xml_file.write("        </Stack>\n")
-    xml_file.write("    </STACKS>\n")
-    xml_file.write("</TeraStitcher>\n")
-
-fusion_folder = root_folder + "/LR_fusion/"
-os.chdir(fusion_folder)
-
-if os.path.exists(meta_name):
-    with open(meta_name,'a') as meta_file:
-        meta_file.write("[x positions_%s] : %r\n" % (illumination_side,x_pos_all))
-else:    
-    with open(meta_name,'w') as meta_file:
-        meta_file.write("[pixel size of x (µm)] : %.3f\n"%dim_H)
-        meta_file.write("[pixel size of y (µm)] : %.3f\n"%dim_V)
-        meta_file.write("[z step size (µm)] : %.3f\n"%dim_D)
-        meta_file.write("[pixel counts in x] : %d\n"%x_pixel_count)
-        meta_file.write("[pixel counts in y] : %d\n"%y_pixel_count)
-        meta_file.write("[x positions_%s] : %r\n" % (illumination_side,x_pos_all))
+    meta_data = dict()
+    meta_data["pixel size of x (um)"] = dim_H
+    meta_data["pixel size of y (um)"] = dim_V
+    meta_data["z step size (um)"] = dim_D
+    meta_data["pixel counts in x"] = x_pixel_count
+    meta_data["pixel counts in y"] = y_pixel_count
+    side_position = "x_position_" + illumination_side
+    meta_data[side_position] = x_pos_all
+    return meta_data
+    """
+        with open(meta_name,'w') as meta_file:
+            meta_file.write("[pixel size of x (µm)] : %.3f\n"%dim_H)
+            meta_file.write("[pixel size of y (µm)] : %.3f\n"%dim_V)
+            meta_file.write("[z step size (µm)] : %.3f\n"%dim_D)
+            meta_file.write("[pixel counts in x] : %d\n"%x_pixel_count)
+            meta_file.write("[pixel counts in y] : %d\n"%y_pixel_count)
+            meta_file.write("[x positions_%s] : %r\n" % (illumination_side,x_pos_all))
+    """
