dedb0370e1dc75e977f865c097eb7e15db3bcf81
tzlohn <57408685+tzlohn@users.noreply.github.com>
2021-04-30 12:58:00 +0200

    It only renames tif file now

--- rename_by_meta.py
+++ rename_by_meta.py
@@ -8,8 +8,9 @@ root.withdraw()
 
 folder = filedialog.askdirectory()
 os.chdir(folder)
-all_meta_file = glob.glob("*_meta.txt")
-all_raw_file = glob.glob("*.raw")
+all_meta_file = glob.glob("*tif_meta.txt")
+#all_raw_file = glob.glob("*.raw")
+all_raw_file = glob.glob("*.tif")
 
 for a_meta in all_meta_file:
     meta = open(a_meta,'r')
@@ -26,13 +27,13 @@ for a_meta in all_meta_file:
     [n,mag] = find_key_from_meta(all_lines,"Zoom")
     [n,side] = find_key_from_meta(all_lines,"Shutter")
 
-    name = "X"+x_pos+"_Y"+y_pos+"_"+laser+"_nm_"+filters+"_"+mag+"_"+side+".raw"
+    name = "X"+x_pos+"_Y"+y_pos+"_"+laser+"_nm_"+filters+"_"+mag+"_"+side+".tif"
 
     name_prefix = a_meta[0:len(a_meta)-9]
     n = 0
-    while name_prefix != all_raw_file[n] and n < len(all_raw_file)-1 :
+    while name_prefix != all_raw_file[n] and n < len(all_raw_file) :
         n = n+1
-    if n >= len(all_raw_file)-1:
+    if n >= len(all_raw_file):
         print("the image file %s can't be found"%name_prefix)
     else:
         image_name = all_raw_file[n]
