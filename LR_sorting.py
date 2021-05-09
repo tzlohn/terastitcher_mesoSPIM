97a1f6adbe1e6b6d38ec1fde6bcaff5b8bbb6329
tzlohn <57408685+tzlohn@users.noreply.github.com>
2021-05-09 19:41:02 +0200

    Trying different ways of tif conversion, cascade is the best

--- LR_sorting.py
+++ LR_sorting.py
@@ -41,17 +41,18 @@ def progress_bar(datatype):
     sys.stdout.write("\r{0}| ({1}/{2})".format(">"*p+"="*(100-p),n,total_length))
     sys.stdout.flush()
 
-def save2tif(raw_list,working_folder,datatype):
+def save2tif(a_raw_file,working_folder,datatype):
 
     # magic number zone #
     background_intensity = 120
-
+    
+    """
     a_raw_file = raw_list.pop(0)
     if raw_list:
         next_process = Process(target = save2tif, args=(raw_list,working_folder,datatype))
-     
+    """
     progress_bar(datatype)
-      
+
     its_meta_file = a_raw_file + "_meta.txt"
     # get pixel number, pixel size and dimensions for memmap to load the image
     dim_names = ['z_planes','y_pixels','x_pixels']
@@ -102,10 +103,10 @@ def save2tif(raw_list,working_folder,datatype):
             if is_scanned == False:
                 TFF.imwrite(new_tif_name,data = im, bigtiff = True)
             new_meta_name = its_meta_file
-        
+        """
         if raw_list:
             next_process.start()
-
+        """
         if illumination_side == "Left":
             #shutil.move(new_tif_name, working_folder+"/Left")
             os.rename(new_tif_name, working_folder+"/Left/"+new_tif_name)    
@@ -116,13 +117,14 @@ def save2tif(raw_list,working_folder,datatype):
             shutil.move(new_meta_name, working_folder+"/Right")
         
         progress_bar(datatype)
-
+    """
     else:
         if raw_list:
             next_process.start()
     
     if raw_list:
         next_process.join()
+    """
 
 def sortLR(working_folder, datatype = "mesoSPIM raw"):
     print("Left-Right sorting starts...")
@@ -144,9 +146,13 @@ def sortLR(working_folder, datatype = "mesoSPIM raw"):
         msgWindow.setIcon(QtWidgets.QMessageBox.Warning)
         msgWindow.setWindowTitle("Can't find images with %s format"%datatype)
         msgWindow.setText("Can't find images with %s format,\n please check whether the image format is correct"%datatype)
+        msgWindow.exec_()
         return 0
-    core_no = int(virtual_memory().free/a_file_size)    
-
+     
+    core_no = int(virtual_memory().free/a_file_size)
+    if core_no > mp.cpu_count()-1:
+        core_no =  mp.cpu_count()-1   
+    """
     round_no = (len(all_raw_files)//core_no)+1
     for a_round in range(round_no):
         if core_no*(a_round+1) < len(all_raw_files):
@@ -154,6 +160,29 @@ def sortLR(working_folder, datatype = "mesoSPIM raw"):
         else:
             working_raw_list = all_raw_files[core_no*a_round:len(all_raw_files)]
         save2tif(working_raw_list,working_folder,datatype)
+    """
+    """
+    for a_raw_file in all_raw_files:
+        save2tif(a_raw_file,working_folder,datatype)    
+    """
+    
+    pool_input = [(a_raw_file,working_folder,datatype) for a_raw_file in all_raw_files]
+    with get_context("spawn").Pool(processes=core_no) as pool:
+        try:
+            result = pool.starmap(save2tif,pool_input)
+            pool.close()
+        except:
+            pool.close()
+            pool.join()
+            if datatype == "mesoSPIM raw":
+                all_tif = glob.glob("*.tif")
+                for a_tif in all_tif:
+                    os.remove(a_tif)
+                all_meta_tif = glob.glob("*.tif_meta.txt")
+                for a_meta_tif in all_meta_tif:
+                    os.remove(a_meta_tif)
+        pool.join()
+    
     print("Left-right file sorting is finished.")
 
 if __name__ == "__main__":
